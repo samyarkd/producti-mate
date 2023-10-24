c99d9efba0f6d67a1ade773deb9b4b0506f234d9
Samyar <samyarkd@gmail.com>
2023-10-24 21:00:01 +0330

    chore: postcss

--- apps/producti-mate/src/main.tsx
+++ apps/producti-mate/src/main.tsx
@@ -1,22 +1,22 @@
-import { StrictMode } from "react";
-import * as ReactDOM from "react-dom/client";
+import { ReactElement, StrictMode } from "react"
+import * as ReactDOM from "react-dom/client"
 
-import { RouterProvider } from "@tanstack/react-router";
-import { SDKProvider } from "@twa.js/sdk-react";
-import { Loader } from "./components/loader";
-import { router } from "./components/router";
-import AxiosClient from "./lib/services/axiosClient";
+import { RouterProvider } from "@tanstack/react-router"
+import { SDKProvider } from "@twa.js/sdk-react"
+import { Loader } from "./components/loader"
+import { router } from "./components/router"
+import AxiosClient from "./lib/services/axiosClient"
 
 // Register your router for maximum type safety
 declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router;
+    router: typeof router
   }
 }
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
-);
+)
 root.render(
   <StrictMode>
     <SDKProvider>
@@ -26,5 +26,5 @@ root.render(
         </AxiosClient>
       </Loader>
     </SDKProvider>
-  </StrictMode>,
-);
+  </StrictMode> as ReactElement,
+)
